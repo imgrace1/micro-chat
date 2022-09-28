e593cec9c0cfb88d0bd627e169d2804348a67cb0
imgrace1 <114506189+imgrace1@users.noreply.github.com>
2022-09-28 12:31:12 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(":)")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+basic.showString("Hello!")
+radio.setGroup(1)
